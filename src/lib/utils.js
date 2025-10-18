5779fe8164cebbb099cf3e32e21e09da17d69f79
phucanh <phucanh05022004@gmail.com>
2025-10-18 22:05:59 +0700

    fix login + add signup

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -5,3 +5,35 @@ export function formatMessageTimestamp(date) {
     hour12: false,
   });
 }
+
+export function getToken(){
+  return sessionStorage.getItem("token");
+}
+
+export function setToken(token){
+  sessionStorage.setItem("token", token);
+}
+
+export const handleInputChange = (setFormData, setErrors) => (e) => {
+  const { name, value } = e.target;
+  setFormData(prevData => ({ ...prevData, [name]: value }));
+  setErrors(prevErrors => {
+    if (prevErrors[name]) {
+      const newErrors = { ...prevErrors };
+      delete newErrors[name];
+      return newErrors;
+    }
+    return prevErrors;
+  });
+};
+
+export const onFileChange = (setImageToCrop) => (e) => {
+  if (e.target.files && e.target.files.length > 0) {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+      setImageToCrop(reader.result);
+    });
+    reader.readAsDataURL(file);
+  }
+};
